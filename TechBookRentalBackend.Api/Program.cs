209d3686676989a2b2203e342c0f66acfe47d61d
aleke <aleke.rysbek@mail.ru>
2025-12-15 14:36:20 +0500

    added swagger

--- TechBookRentalBackend.Api/Program.cs
+++ TechBookRentalBackend.Api/Program.cs
@@ -1,15 +1,30 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TechBook Rental API",
+        Version = "v1",
+        Description = "API for TechBook Rental Backend Service"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "TechBook Rental API v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
